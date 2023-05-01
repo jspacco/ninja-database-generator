d652eb7c9ae7d4468a9c10a968afd2fa7d094273
Jaime Spacco <jspacco@knox.edu>
2023-05-01 12:23:45 -0500

    added more stuff

--- ninjas.sh
+++ ninjas.sh
@@ -1,8 +1,22 @@
 #!/bin/bash
 
-for i in {1..34}; do
-    ruby ninjas2.rb 200000 $i > ninjas.sql
-    sqlite3 db/ninjas$i.db < ninjas.sql
+filename="a.txt"
+
+i=1
+if [ -f "$filename" ]; then
+    while read line; do
+        echo $line $i
+        ruby ninjas2.rb 10 $i > ninjas.sql
+        sqlite3 db3/$line.db < ninjas.sql
+        ((i++))
+    done < "$filename"
+fi
+
+exit
+
+for i in {36..36}; do
+    #ruby ninjas2.rb 300000 $i > ninjas.sql
+    sqlite3 db2/ninjas$i.db < ninjas.sql
     echo $i
 done
 
